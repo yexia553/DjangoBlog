ddc77cdc1bbd14bbc7d0694081c8d301fc53673c
yexia553 <1906390603@qq.com>
2021-05-23 20:51:43 +0800

    添加gunicron的配置

---
+++ gunicorn_start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+NAME="Blog"
+DJANGODIR=/app/DjangoBlog #Django project directory
+USER=ubuntu # the user to run as
+GROUP=ubuntu # the group to run as
+NUM_WORKERS=1 # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=DjangoBlog.settings # which settings file should Django use
+DJANGO_WSGI_MODULE=DjangoBlog.wsgi # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source /app/env/blog/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec /app/env/blog/bin/gunicorn  ${DJANGO_WSGI_MODULE}:application \
+	--name $NAME \
+	--workers $NUM_WORKERS \
+	--user=$USER --group=$GROUP \
+	--log-level=debug \
+	--log-file=-
