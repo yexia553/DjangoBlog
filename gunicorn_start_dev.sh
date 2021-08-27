77584fee57af4f6cd441253d63498e602482727c
zhixiang-pan <zhixiang_pan@trendmicro.com>
2021-08-27 17:49:11 +0800

    添加开发环境调试信息

--- gunicorn_start_dev.sh
+++ gunicorn_start_dev.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 NAME="Blog"
-DJANGODIR=/app/DjangoBlog #Django project directory
-USER=ubuntu # the user to run as
-GROUP=ubuntu # the group to run as
+DJANGODIR=/home/zhixiang_pan/learningspace/DjangoBlog #Django project directory
+USER=zhixiang_pan # the user to run as
+GROUP=zhixiang_pan # the group to run as
 NUM_WORKERS=1 # how many worker processes should Gunicorn spawn
 DJANGO_SETTINGS_MODULE=DjangoBlog.settings # which settings file should Django use
 DJANGO_WSGI_MODULE=DjangoBlog.wsgi # WSGI module name
@@ -11,7 +11,7 @@ echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-source /app/env/blog/bin/activate
+source /home/zhixiang_pan/learningspace/DjangoBlog/venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
@@ -21,7 +21,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec /app/env/blog/bin/gunicorn  ${DJANGO_WSGI_MODULE}:application \
+exec /home/zhixiang_pan/learningspace/DjangoBlog/venv/bin/gunicorn  ${DJANGO_WSGI_MODULE}:application \
 	--name $NAME \
 	--workers $NUM_WORKERS \
 	--user=$USER --group=$GROUP \
